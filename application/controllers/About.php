7f769f451be2b8d63719fa2710951235191b968e
3rafanan <3rafanan@gmail.com>
2015-01-17 16:24:52 -0800

    Initial commit
    Signed-off-by: 3rafanan <3rafanan@gmail.com>

---
+++ application/controllers/About.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		$this->load->view('about');
+	}
+}
+
+/* End of file About.php */
+/* Location: ./application/controllers/About.php */
---
+++ application/controllers/Features.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Features extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		$this->load->view('features');
+	}
+}
+
+/* End of file Features.php */
+/* Location: ./application/controllers/Features.php */
